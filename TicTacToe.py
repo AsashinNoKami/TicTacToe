4a21b648bd3f5f6384d705f78f8ba199afa7fcab
AsashinNoKami <pacheco.jaguar@gmail.com>
2024-02-12 20:20:59 -0800

    new file:   TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,17 @@
+def board():
+    board = """
+    Este sera nuestro tablero de juego
+    
+    1 | 2 | 3
+    --|---|--
+    4 | 5 | 6
+    --|---|--
+    7 | 8 | 9
+    
+    *Instrucciones:
+    1. Insertar un numero entre 1 al 9 para elejir la posicion.
+    2. Se tienen que llenar las 9 posiciones para terminar el juego.
+    3. Usted iniciara el juego.
+    """
+    print(board)
+board()
