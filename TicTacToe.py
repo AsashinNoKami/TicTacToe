f47c4e033f92fdddcdce65be15bb30d0e7524470
AsashinNoKami <pacheco.jaguar@gmail.com>
2024-02-23 17:12:53 -0800

    modified:   TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,3 +17,27 @@ def board():
     """
     print(board)
 board()
+
+tic = {"1","2","3","4","5","6","7","8","9"}
+
+#posibles soluciones Format or Concatenar
+#yes_votes = 42_572_654
+#no_votes = 43_132_495
+#percentage = yes_votes / (yes_votes + no_votes)
+#'{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage)
+#' 42572654 YES votes  49.67%'
+def update():
+    print("""
+    Elige una posicion 
+          
+    {tic[0]} | {tic[1]} | {tic[2]}
+    --|---|--
+    {tic[3]} | {tic[4]} | {tic[5]}
+    --|---|--
+    {tic[6]} | {tic[7]} | {tic[8]}
+    
+          """)
+
+while True:
+    update()
+
